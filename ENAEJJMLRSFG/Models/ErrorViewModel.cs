39c05fe30bb4bb69730c9be7521a92630cc88a83
Emerson Neftali Aguilar Erazo <ae24001@esfe.agape.edu.sv>
2024-03-21 08:26:01 -0600

    Agregar archivos de proyecto.

---
+++ ENAEJJMLRSFG/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ENAEJJMLRSFG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

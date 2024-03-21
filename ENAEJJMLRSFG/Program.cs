003654439ec320008d2915ada9f2d4b91abde71f
Emerson Neftali Aguilar Erazo <ae24001@esfe.agape.edu.sv>
2024-03-21 08:41:19 -0600

    Configuracion De Program Y Appsettings

--- ENAEJJMLRSFG/Program.cs
+++ ENAEJJMLRSFG/Program.cs
@@ -1,7 +1,13 @@
+using ENAEJJMLRSFG.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ENAEJJMLRSFGContext>(options =>
+      options.UseSqlServer(builder.Configuration.GetConnectionString("Cnn")));
 
 var app = builder.Build();
 

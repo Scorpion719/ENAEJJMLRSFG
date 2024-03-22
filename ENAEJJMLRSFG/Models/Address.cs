a37b4727302e8944fad0512b0c5ff6e01477dd4f
Emerson neftali Aguilar Erazo <ae24001@esfe.agape.edu.sv>
2024-03-21 21:25:46 -0600

    Anotaciones completado

--- ENAEJJMLRSFG/Models/Address.cs
+++ ENAEJJMLRSFG/Models/Address.cs
@@ -10,16 +10,28 @@ namespace ENAEJJMLRSFG.Models
         public int Id { get; set; }
 
         [Display(Name = "Cliente")]
+        [Required(ErrorMessage = "El campo Cliente")]
+        
         public int CustomerId { get; set; }
         [Display(Name = "Calle")]
+        [Required(ErrorMessage = "El campo Calle")]
+        [StringLength(100)]
         public string Street { get; set; } = null!;
         [Display(Name = "Ciudad")]
+        [Required(ErrorMessage = "El campo Ciudad")]
+        [StringLength(50)]
         public string City { get; set; } = null!;
         [Display(Name = "Estado")]
+        [Required(ErrorMessage = "El campo Estado")]
+        [StringLength(50)]
         public string State { get; set; } = null!;
         [Display(Name = "País")]
+        [Required(ErrorMessage = "El campo País")]
+        [StringLength(50)]
         public string Country { get; set; } = null!;
         [Display(Name = "Código postal")]
+        [Required(ErrorMessage = "El campo postal")]
+        [StringLength(20)]
         public string? PostalCode { get; set; }
 
         public virtual Customer Customer { get; set; } = null!;
--- ENAEJJMLRSFG/Models/User.cs
+++ ENAEJJMLRSFG/Models/User.cs
@@ -9,20 +9,26 @@ namespace ENAEJJMLRSFG.Models
     {
         public int Id { get; set; }
         [Display(Name = "Nombre de usuario")]
-        [StringLength(50
-            )]
+        [StringLength(50)]
+        [Required(ErrorMessage = "El campo Nombre de usuario ")]
         public string UserName { get; set; } = null!;
         [Display(Name = "Contraseña")]
         [StringLength(32, MinimumLength = 6, ErrorMessage = "La contraseña debe tener entre 6 y 100 caracteres.")]
+        [Required(ErrorMessage = "El campo Contraseña ")]
         public string Password { get; set; } = null!;
         [Display(Name = "Correo electrónico")]
-        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "El Correo Email debe tener un formato válido.")]
+        [Required(ErrorMessage = "El campo Correo electrónico ")]
+        [StringLength(100)]
         public string Email { get; set; } = null!;
         [Display(Name = "Estado")]
+        [Required(ErrorMessage = "El campo Estado ")]
         public byte Status { get; set; }
         [Display(Name = "Imagen")]
+        [Required(ErrorMessage = "El campo Imagen")]
         public byte[]? Image { get; set; }
         [Display(Name = "Papel")]
+        [Required(ErrorMessage = "El campo Papel")]
         public int RoleId { get; set; }
         [NotMapped] // Esta propiedad no será mapeada a la base de datos
         [Compare("Password", ErrorMessage = "La confirmación de contraseña no coincide.")]
